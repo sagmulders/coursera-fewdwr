c8ff8fa5f221bc90a56d351fb3c0474bfd7ae03c
Serge Mulders <sagmulders@gmail.com>
2018-08-20 21:14:13 +0200

    uncontrolled forms

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,24 +1,39 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Nav, NavbarToggler, NavItem, Collapse, Jumbotron } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavbarToggler, NavItem, Collapse, Jumbotron, Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 export default class Header extends Component {
 
-    constructor (props) {
+    constructor(props) {
         super(props)
 
-        this.state = { 
-            isNavOpen: false
+        this.state = {
+            isNavOpen: false,
+            isModalOpen: false
         };
 
         this.toggleNav = this.toggleNav.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
     }
 
-     toggleNav() {
-         this.setState({isNavOpen: !this.state.isNavOpen});
-     }
+    toggleNav() {
+        this.setState({ isNavOpen: !this.state.isNavOpen });
+    }
 
-    render(){
+    toggleModal() {
+        this.setState({ isModalOpen: !this.state.isModalOpen });
+    }
+
+    handleLogin(evt){
+        this.toggleModal();
+
+        alert(this.username.value + " " + this.password.value);
+
+        evt.preventDefault();
+    }
+
+    render() {
         return (
             <React.Fragment>
                 <Navbar dark expand="md">
@@ -29,31 +44,38 @@ export default class Header extends Component {
                         </NavbarBrand>
                         <Collapse isOpen={this.state.isNavOpen} navbar >
 
-                        <Nav navbar>
-                            <NavItem>
-                                <NavLink className="nav-link" to="/home"> 
-                                    <span className="fa fa-home fa-lg"></span> Home
+                            <Nav navbar>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/home">
+                                        <span className="fa fa-home fa-lg"></span> Home
                                 </NavLink>
-                            </NavItem>
+                                </NavItem>
 
-                            <NavItem>
-                                <NavLink className="nav-link" to="/aboutus"> 
-                                    <span className="fa fa-info fa-lg"></span> About us
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/aboutus">
+                                        <span className="fa fa-info fa-lg"></span> About us
                                 </NavLink>
-                            </NavItem>
+                                </NavItem>
 
-                            <NavItem>
-                                <NavLink className="nav-link" to="/menu"> 
-                                    <span className="fa fa-list fa-lg"></span> Menu
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/menu">
+                                        <span className="fa fa-list fa-lg"></span> Menu
                                 </NavLink>
-                            </NavItem>
+                                </NavItem>
 
-                            <NavItem>
-                                <NavLink className="nav-link" to="/contactus"> 
-                                    <span className="fa fa-address-card fa-lg"></span> Contact us
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/contactus">
+                                        <span className="fa fa-address-card fa-lg"></span> Contact us
                                 </NavLink>
-                            </NavItem>
-                        </Nav>
+                                </NavItem>
+                            </Nav>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <Button outline onClick={this.toggleModal}>
+                                        <span className="fa fa-sign-in fa-lg">Login</span>
+                                    </Button>
+                                </NavItem>
+                            </Nav>
                         </Collapse>
                     </div>
                 </Navbar>
@@ -67,6 +89,30 @@ export default class Header extends Component {
                         </div>
                     </div>
                 </Jumbotron>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}> Login</ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username" innerRef={(input) => this.username = input } />
+                            </FormGroup>
+
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password" innerRef={(input) => this.password = input } />
+                            </FormGroup>
+
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="remember" innerRef={(input) => this.remember = input } />
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">Login</Button>
+                        </Form>
+                    </ModalBody>
+                </Modal>
             </React.Fragment>
         );
     }
